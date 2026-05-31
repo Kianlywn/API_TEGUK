4f270893f9756b9a0403d55d1f796db214415475
Kianlywn <faizbussines86@gmail.com>
2026-05-31 19:26:19 +0700

    fix swagger

--- Program.cs
+++ Program.cs
@@ -111,11 +111,8 @@ builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
